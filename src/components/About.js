4a6e32624fe03d897bd0060c2961c5e6d4666abb
Andrew Moon <andrewjm80@gmail.com>
2020-03-26 14:13:31 -0500

    gitcommit1

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="App">
+      <h1 className="center"> About Page: </h1>
+      The Prayer Project is an accumulation of prayers from saints and holy
+      people. The prayerlist page contains a search bar that allows the users to
+      search by the saint's first name. There is also a page that provides
+      additional information about the saints including feast days and
+      Patronage.
+    </div>
+  );
+};
---
+++ src/components/posts/PrayerList.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import prayer from '../../data/prayers.json';
+import search from './Search';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  paper: {
+    maxWidth: 650,
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+    backgroundColor: 'lightblue'
+  }
+}));
+
+const PostList = () => {
+  const classes = useStyles();
+  //const [post, setPost] = useState('');
+  const [term, setTerm] = useState('');
+
+  const searchHandler = e => {
+    setTerm(e.target.value); //outputs watever the input is inputted
+  };
+
+  //const { term, post } = this.state; //destructuring
+  return (
+    <div className="App">
+      <h1>Prayer Project</h1>
+      <form>
+        Search By Title:
+        <input
+          className="Input"
+          type="text"
+          onChange={searchHandler}
+          value={term}
+        />
+      </form>
+      <div>
+        {/* {this.state.post.filter(search(this.state.term)).map((a) =>{ */}
+        {prayer.filter(search(term)).map(a => {
+          return (
+            <Paper
+              key={a.id}
+              direction="column"
+              alignitems="center"
+              justify="center"
+              className={classes.paper}
+            >
+              <Grid
+                container
+                spacing={0}
+                direction="column"
+                className={classes.root}
+              >
+                <List className={classes.root}>
+                  <ListItem
+                    variant="h2"
+                    justify="center"
+                    className={classes.root}
+                  >
+                    <b>Title: {a.title}</b>
+                  </ListItem>
+                  <hr />
+                  <br />
+                  <ListItem
+                    variant="h4"
+                    justify="center"
+                    className={classes.root}
+                  >
+                    <b>Prayer- {a.body}</b>
+                  </ListItem>
+                  <br />
+                  <ListItem
+                    variant="h4"
+                    justify="center"
+                    className={classes.root}
+                  >
+                    <b>
+                      Source-{' '}
+                      <a
+                        style={{ color: 'black' }}
+                        href={a.source}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Link
+                      </a>
+                    </b>
+                  </ListItem>
+                </List>
+              </Grid>
+            </Paper>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
